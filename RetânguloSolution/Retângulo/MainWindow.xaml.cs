4d9e01420d6303b4fdd8b7ada1241d113e844099
Joel Victor <jNull.rx@hotmail.com>
2019-08-21 18:00:05 -0300

    fiz agr

---
+++ RetânguloSolution/Retângulo/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Retângulo
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+
+    class Retangulo
+    {
+        private double b, h;
+        public void SetBase(double b)
+        {
+            this.b = b;
+        }
+        public void SetAltura(double h)
+        {
+            this.h = h;
+        }
+        public double GetBase()
+        {
+            return b;
+        }
+        public double GetAltura()
+        {
+            return h;
+        }
+        public double CalcArea()
+        {
+            return b * h;
+        }
+        public double CalcDiag()
+        {
+            return Math.Sqrt(Math.Pow(b, 2) + Math.Pow(h, 2));
+        }
+    }
+}
