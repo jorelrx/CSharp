43b171697c72ad5057bebcc8a1c41ee4cbf92472
Joel Victor <jNull.rx@hotmail.com>
2019-08-21 15:46:26 -0300

    Tudo certo 2

---
+++ EsferaSolution/Esfera/Esfer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esfera
+{
+    class Esfer
+    {
+        private double raio;
+        public void SetRaio(double raio)
+        {
+            this.raio = raio;
+        }
+        public double GetRaio()
+        {
+            return raio;
+        }
+        public double CalcArea()
+        {
+            return 4 * Math.PI * Math.Pow(raio,2);
+        }
+        public double CalcVolume()
+        {
+            return 4 / 3 * Math.PI * Math.Pow(raio, 3);
+        }
+    }
+}
